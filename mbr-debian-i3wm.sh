29139e7e65292260463c43c3faad99feb5c37cd0
Julio <85705200+juliodejong@users.noreply.github.com>
2023-07-20 00:47:58 +0300

    Add files via upload

---
+++ mbr-debian-i3wm.sh
@@ -0,0 +1,3 @@
+apt update 
+apt upgrade
+apt install i3 xorg lightdm slick-greeter lightdm-settings lxappearance nitrogen pcmanfm firefox pulseaudio alsa-utils pavucontrol terminator htop neofetch xfce4-screenshooter -y
