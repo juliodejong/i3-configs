514250da11122098288e654bbc243afcab9b3b0d
Julio <85705200+juliodejong@users.noreply.github.com>
2023-07-20 01:16:11 +0300

    Delete mbr-debian-i3wm.sh

--- mbr-debian-i3wm.sh
+++
@@ -1,3 +0,0 @@
-apt update 
-apt upgrade
-apt install i3 xorg lightdm slick-greeter lightdm-settings lxappearance nitrogen pcmanfm firefox pulseaudio alsa-utils pavucontrol terminator htop neofetch xfce4-screenshooter -y
