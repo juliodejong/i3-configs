841168dc97af8439beed3ecbcc7d7f452fbd91ff
Julio <85705200+juliodejong@users.noreply.github.com>
2023-07-31 17:40:59 +0300

    Update i3-adds.sh

--- i3-adds.sh
+++ i3-adds.sh
@@ -1,5 +1,16 @@
+echo 'Installing Picom , i3-gaps...'
 pacman -S picom i3-gaps 
+echo 'Installing autotiling(Requires aur)'
 yay -S autotiling
-cd Downloads
+echo 'Installing Bumblebee Status...'
 git colne https://github.com/tobi-wan-kenobi/bumblebee-status
-echo 'copy the file to .config' 
+echo '!!Copy the file to .config' 
+echo 'Installing xborder...'
+git clone https://github.com/deter0/xborder
+cd xborder
+echo 'Installing xborder essentials...'
+pacman -S python3-pip
+chmod +x xborders
+pip install -r requirements.txt
+echo 'type ./xborders --help'
+echo 'done'
